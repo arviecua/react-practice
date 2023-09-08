efbb7fb77da03a99a1f5964bf455135f7fee9c01
arviecua <141235475+arviecua@users.noreply.github.com>
2023-09-08 18:21:35 +0800

    practicing some JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Nav from './nav/Nav.js';
+import Footer from './components/Footer.js';
 
 const App = () => {
   return <>
             <Nav />
+            <main>
+              This is the main div
+            </main>
+            <Footer />
         </>;
 }
 
