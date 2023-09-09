3c7aa85e3ff091e73b562d463444d8d509f2ca84
arviecua <141235475+arviecua@users.noreply.github.com>
2023-09-09 18:03:55 +0800

    updated the Jsx element property in route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Nav from './nav/Nav.js';
-import Footer from './components/Footer.js';
+import About from './about/About'
+import Skills from './skills/Skills'
+import Projects from './projects/Projects'
+import Contact from './contact/Contact'
+
 
 const App = () => {
-  return <>
-            <Nav />
-            
-        </>;
+  return (
+    <Router>
+       <Nav />
+        <Routes>
+          <Route path="/" element={<About/> } />
+          <Route path="/skills" element={<Skills/>} />
+          <Route path="/projects" element={<Projects/> } />
+          <Route path="/contact" element={<Contact/> } />
+        </Routes>
+    </Router>
+  )
 }
 
 export default App;
+
